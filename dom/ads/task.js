b789a9b6f434d58c0346e655f443d9d023f77d6c
alle <alle-udal@yandex.ru>
2024-11-05 07:06:45 +0300

    готово

---
+++ dom/ads/task.js
@@ -0,0 +1,16 @@
+function startRotator(rotator) {
+    const cases = rotator.querySelectorAll('.rotator__case');
+    let i = 0;
+    setInterval(() => {
+        cases[i].classList.remove('rotator__case_active');
+        i = (i + 1) % cases.length;
+        cases[i].classList.add('rotator__case_active');
+    }, 1000);
+}
+
+
+
+const rotators = document.querySelectorAll('.rotator');
+for (const rotator of rotators) {
+    startRotator(rotator);
+}
